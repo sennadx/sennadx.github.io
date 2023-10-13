9cd1a0430098c437a2465e9f54563691ab2d191c
Witallo <witallo.oliveira@dock.tech>
2023-10-12 21:29:30 -0300

    chore: added statics for logo and icons

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'SDX',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -70,10 +70,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        //title: 'Developer Experience',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Senna Developer Experience',
+          src: 'sdx/2b75c29183b9a12f21fefee18587e07c.png',
         },
         items: [
           {
